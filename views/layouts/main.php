4a7477ed0823cfd2e16fe82c9eba9c5cba9ede1f
mbn7938 <Utem1234>
2018-10-11 11:20:29 +0800

    change main navigation

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -35,24 +35,26 @@ AppAsset::register($this);
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
+
+
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+            Yii::$app->user->can("admin") ?
+                ['label' => 'Manage', 'items' => [
+                    ['label' => 'Manage User', 'url' => ['/user/admin']],
+                    ['label' => 'Profiles', 'url' => ['/user/profile']],
+
+                ]]:'',
+            Yii::$app->user->isGuest ?
+                ['label' => 'Login', 'url' => ['/user/login']] : // or ['/user/login-email']
+                ['label' => 'Logout (' . Yii::$app->user->displayName . ')',
+                    'url' => ['/user/logout'],
+                    'linkOptions' => ['data-method' => 'post']],
+
         ],
     ]);
     NavBar::end();
